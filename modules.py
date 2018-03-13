90435fba6c030c640c9111556c1b083042c5e504
thang nguyen dinh <thangyk97@gmail.com>
2018-03-13 11:22:41 +0700

    all titlt for figure

--- modules.py
+++ modules.py
@@ -14,7 +14,7 @@ class function(object):
     @param: 3darray images, 1darray labels, int label
 
     """
-    def display_images_has_label(images, labels, label):
+    def display_images_has_label(images, labels, label, title = ""):
         limit = 24
         index = np.where(labels == label)[0]
         np.random.shuffle(index)
@@ -22,10 +22,12 @@ class function(object):
         images_part = images[index]
 
         plt.figure(figsize=(15, 5))
+        plt.suptitle(title)
         i = 1
         for image in images_part:
             plt.subplot(3, 8, i)
             plt.axis('off')
+
             i += 1
             plt.imshow(image)
         plt.show()
